012f765cde6e55dc0d8e10b8d3ca9ddb23a19d6c
A99barnsey <33737199+A99barnsey@users.noreply.github.com>
2018-04-19 11:51:40 -0500

    'first'

---
+++ website_5/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from basic_app import views
+
+
+#template URLs
+
+app_name = 'basic_app'
+
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='user_login'),
+]
